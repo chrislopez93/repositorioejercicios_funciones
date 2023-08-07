cd01de5a455a80c87965b86c3c6ba5bcb65043b8
chrislopez93 <sinretel@gmail.com>
2023-08-07 01:39:53 -0400

    añadido de forde + función js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@ function pintar(color){
     colorear = document.querySelector(".pintado");
     colorear.style.backgroundColor = color;
 }
+
+function agregarBordes(elementId){
+    elemento = document.querySelector('#' + elementId);
+    elemento.style.border = "solid 1px black";
+}
